519b1eba13f0e3be30430a9b30396b09b68e9b1e
jpgonzalezo <juan.gonzalez.o@usach.cl>
2019-12-15 23:08:42 -0300

    ADD: base angular

---
+++ Front/src/app/app.routing.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes =[
+    { path: 'profile',     component: ProfileComponent },
+    { path: 'login',          component: LoginComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: true
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
