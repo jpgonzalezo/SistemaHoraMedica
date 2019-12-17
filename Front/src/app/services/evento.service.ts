3b3c437c3264812dbef846090dd398aab34b9214
jpgonzalezo <juan.gonzalez.o@usach.cl>
2019-12-17 12:57:05 -0300

    UPDATE: Servicio para eliminar reserva

--- Front/src/app/services/evento.service.ts
+++ Front/src/app/services/evento.service.ts
@@ -24,12 +24,12 @@ export class EventoService {
         return this.httpClient.post(`${this.API_URL}/reserves`, JSON.stringify(data), options).pipe(map(res => res))
     }
 
-    deleteEvento(id:string,token:string){
-        const headers = new HttpHeaders({ 'Content-Type': 'application/json' ,'auth-token':token});
+    deleteEvento(id:string){
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json'});
         const options = {
             headers: headers
         }
-        return this.httpClient.delete(`${this.API_URL}/eventos/${id}`,options).pipe(map(res => res))
+        return this.httpClient.delete(`${this.API_URL}/reserves/${id}`,options).pipe(map(res => res))
     }
 
     getSolicitudesEventos(token:string){
