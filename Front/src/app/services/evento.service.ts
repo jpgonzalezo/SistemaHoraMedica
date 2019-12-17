008fab540a7cfa8417d8c7ec8df93ccd3e115bfd
jpgonzalezo <juan.gonzalez.o@usach.cl>
2019-12-17 16:39:37 -0300

    ADD: Servicio para traer reservas de usuario

--- Front/src/app/services/evento.service.ts
+++ Front/src/app/services/evento.service.ts
@@ -16,12 +16,28 @@ export class EventoService {
         return this.httpClient.get(`${this.API_URL}/reserves`,options).pipe(map(res => res))
     }
 
+    getEventosUser(id:string){
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json'});
+        const options = {
+            headers: headers
+        }
+        return this.httpClient.get(`${this.API_URL}/reserves/user/${id}`,options).pipe(map(res => res))
+    }
+
     postEvento(data:any){
         const headers = new HttpHeaders({ 'Content-Type': 'application/json'});
         const options = {
           headers: headers
         }
         return this.httpClient.post(`${this.API_URL}/reserves`, JSON.stringify(data), options).pipe(map(res => res))
+    }
+
+    putEvento(data,id){
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json'});
+        const options = {
+          headers: headers
+        }
+        return this.httpClient.put(`${this.API_URL}/reserves/${id}`, JSON.stringify(data), options).pipe(map(res => res))
     }
 
     deleteEvento(id:string){
